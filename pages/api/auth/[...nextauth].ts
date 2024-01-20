d9e81d2e1587ea47df71aa902fb9623846796ea6
Ali Shariatian <shariatian92@gmail.com>
2024-01-20 17:35:49 +0330

    config: add next auth sign in config

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,74 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import NextAuth, { AuthOptions } from "next-auth";
+import prisma from "@/libs/prismadb";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+
+export const authOptions: AuthOptions = {
+   adapter: PrismaAdapter(prisma),
+   providers: [
+      GithubProvider({
+         clientId: process.env.GITHUB_ID as string,
+         clientSecret: process.env.GITHUB_SECRET as string,
+      }),
+
+      GoogleProvider({
+         clientId: process.env.GOOGLE_CLIENT_ID as string,
+         clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      }),
+
+      CredentialsProvider({
+         name: "customLoginForm",
+         credentials: {
+            email: { label: "email", type: "email" },
+            password: { label: "password", type: "password" },
+         },
+
+         async authorize(customLoginForm) {
+            // if email and user is empty
+            if (!customLoginForm?.email || !customLoginForm?.password) {
+               throw new Error("Invalid Email or Password");
+            }
+
+            // search user in database
+            const user = await prisma.user.findUnique({
+               where: {
+                  email: customLoginForm.email,
+               },
+            });
+
+            // if not exist user
+            if (!user || !user?.hashedPassword) {
+               throw new Error("Email not found! Please SignUp.");
+            }
+
+            const isCorrectPassword = await bcrypt.compare(customLoginForm.password, user.hashedPassword);
+
+            // if password is not correct
+            if (!isCorrectPassword) {
+               throw new Error("Invalid password");
+            }
+
+            // if user Exist in database AND Correct password.
+            return user;
+         },
+      }),
+   ],
+
+   // Specify URLs to be used if we want to create custom sign in, sign out and error pages.
+   pages: {
+      signIn: "/",
+   },
+   // Set debug to true to enable debug messages for authentication and database operations in development mode.
+   debug: process.env.NODE_ENV === "development",
+
+   // jwt: json web token
+   session: {
+      strategy: "jwt",
+   },
+   secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOptions);

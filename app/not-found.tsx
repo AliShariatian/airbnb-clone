8542f796a5e041986b13a8ba281734c822dba7ea
Ali Shariatian <shariatian92@gmail.com>
2024-02-02 22:34:46 +0330

    feat: add error page (error page handling)

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import Heading from "@/components/Heading";
+
+const Page404 = () => {
+   return (
+      <div className="h-[70vh] flex flex-col items-center justify-center">
+         <Heading title="404" subTitle="Page not found!" center />
+      </div>
+   );
+};
+
+export default Page404;
